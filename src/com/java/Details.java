5a662a15452d6b17c038d54c0df7306a7944b185
AswathiMohan23 <achu999here@gmail.com>
2022-10-24 11:19:00 +0530

    [ADDED] : pojo class to store the variables and details

--- src/com/java/Details.java
+++ src/com/java/Details.java
@@ -1,4 +1,71 @@
 package com.java;
 
+import java.time.LocalDate;
+
 public class Details {
+    private String name;
+    private String address;
+    private String zipCode;
+    private String mobileNumber;
+    private String lastName;
+
+    public Details(String name, String address, String zipCode, String mobileNumber, String lastName) {
+        this.name = name;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.mobileNumber = mobileNumber;
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Details{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
 }
